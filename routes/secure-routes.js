880c6f8d6d56ff9768196470713bc82810b1bdc0
hitesh054 <chandnanihitesh0701@gmail.com>
2021-04-22 23:51:19 +0530

    Login and sign up changes

---
+++ routes/secure-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get(
+    '/file-upload',
+    (req, res, next) => {
+        res.json({
+            message: 'You made it to the secure route',
+            user: req.user,
+            token: req.query.secret_token
+        })
+    }
+);
+
+
+module.exports = router;
